df5067ed5a552d316e74aee49e57360524319cf6
Noah Dietz <noahdietz@ITs-MacBook-Air-8.local>
2015-06-16 16:19:44 -0700

    user GET works

--- travel-groups/api/controllers/travel_groups.js
+++ travel-groups/api/controllers/travel_groups.js
@@ -1,8 +1,7 @@
 'use strict';
 
 var util = require('util');
-<<<<<<< HEAD
-var request = require('request')
+
 
 module.exports = {
 	getUserByID: getUserByID,
@@ -14,18 +13,20 @@ function getPlanByID(req, res) {
 	var plan = {
 		"id": "1",
 		"creater": id,
-		group_member: [],
-		transportations: [],
-		"depature": "3:00 pm"
-		"destination": "San Francisco"
+		"group_member": [],
+		"transportations": [],
+		"depature": "3:00 pm",
+		"destination": "San Francisco",
 		"origin": "San Jose"
 	};
 	res.json(plan);
+}
 
 function getUserByID(req, res) {
   var id = req.swagger.params.id.value;
   var user = {
   	'name':'John Doe',
+  	'password':'test1234',
   	'id':id,
   	'plan_id':1234567,
   	'friends':[]
