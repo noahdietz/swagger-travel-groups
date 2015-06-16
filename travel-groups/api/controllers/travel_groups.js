3ee404e45ee879405abd27b079edcff881abb98c
Noah Dietz <noahdietz@ITs-MacBook-Air-8.local>
2015-06-16 15:58:42 -0700

    added getUserByID

--- travel-groups/api/controllers/travel_groups.js
+++ travel-groups/api/controllers/travel_groups.js
@@ -1,3 +1,19 @@
 'use strict';
 
 var util = require('util');
+
+module.exports = {
+	getUserByID: getUserByID
+}
+
+function getUserByID(req, res) {
+  var id = req.swagger.params.id.value;
+  var user = {
+  	'name':'John Doe',
+  	'id':id,
+  	'plan_id':1234567,
+  	'friends':[]
+  };
+
+  res.json(user);
+}
